89f8fec3211d5387506c965de8a2fa558964f44c
Nguyễn Trường Giang <44118083+bingii98@users.noreply.github.com>
2020-06-18 04:35:08 +0700

    dfsd

--- commands/nowplaying.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'nowplaying',
-	description: 'Nhận bài hát đang phát.',
-	execute(message) {
-		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('There is nothing playing.');
-		return message.channel.send(`Now playing: ${serverQueue.songs[0].title}`);
-	},
-};
